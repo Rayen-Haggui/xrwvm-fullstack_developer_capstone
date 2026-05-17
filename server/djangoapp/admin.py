cdb9bbee8cb1b5986ffdfeb91596a0024b6d2b13
Rayen-Haggui <rayen.hakki@gmail.com>
2026-05-17 11:43:03 -0400

    Implement CarMake and CarModel backend architecture, fix admin panel CSRF routing

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,25 @@
-# from django.contrib import admin
-# from .models import related models
-
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 # Register your models here.
 
-# CarModelInline class
+# CarModelInline class allows adding CarModel records directly inside the CarMake page
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 2  # Provides 2 extra blank slots by default to quickly add new models
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year', 'dealer_id')
+    list_filter = ['type', 'year']
+    search_fields = ['name']
 
-# CarMakeAdmin class with CarModelInline
+# CarMakeAdmin class with CarModelInline embedded
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ['name']
+    inlines = [CarModelInline]
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,7 +13,10 @@ from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
+
+# Imported models and initialization script for car features
+from .models import CarMake, CarModel
+from .populate import initiate
 
 
 # Get an instance of a logger
@@ -50,7 +53,7 @@ def logout_user(request):
 def registration(request):
     context = {}
 
-	# Load JSON data from the request body
+    # Load JSON data from the request body
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
@@ -78,6 +81,19 @@ def registration(request):
     else :
         data = {"userName":username,"error":"Already Registered"}
         return JsonResponse(data)
+
+
+# Method to get the list of cars and auto-populate if empty
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 
 
 # # Update the `get_dealerships` view to render the index page with
